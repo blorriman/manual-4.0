ada401e8283cbe4ea46d78ef6e810fd810181725
Bob Lorriman <blorriman@gmail.com>
2021-05-30 15:20:18 -0700

    oops - forgot these ones

---
+++ docs/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Why Use LORROS?
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum at varius vel pharetra vel turpis nunc eget lorem. A iaculis at erat pellentesque adipiscing commodo elit at. Pretium nibh ipsum consequat nisl vel pretium lectus quam. Nulla pharetra diam sit amet nisl. Et netus et malesuada fames ac turpis egestas. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. In tellus integer feugiat scelerisque varius. Egestas sed tempus urna et pharetra pharetra. At auctor urna nunc id cursus metus aliquam eleifend mi.
---
+++ docusaurus.config.js
@@ -0,0 +1,78 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+	title: 'LORROS User Guide',
+	tagline: 'democratic decision making for boards and committees',
+	url: 'https://lorros.com',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'facebook', // Usually your GitHub org/user name.
+	projectName: 'docusaurus', // Usually your repo name.
+	themeConfig: {
+		colorMode: {
+			defaultMode: 'light',
+			disableSwitch: true,
+		},
+		navbar: {
+			logo: {
+				alt: 'LORROS Logo',
+				src: 'img/logo.png',
+			},
+			items: [
+				{
+					type: 'doc',
+					docId: 'intro',
+					position: 'left',
+					label: 'User Manual',
+				},
+				{
+					to: 'administration/intro',
+					activeBasePath: 'administration/intro',
+					label: 'Administrator Manual',
+					position: 'left',
+				},
+				{ to: '/blog', label: 'Best Practices', position: 'left' },
+			],
+		},
+		footer: {
+			style: 'dark',
+			copyright: `Copyright © ${new Date().getFullYear()} LORROS Web Applications Ltd.`,
+		},
+	},
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					// editUrl:
+					// 	'https://github.com/facebook/docusaurus/edit/master/website/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// editUrl:
+					// 	'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			},
+		],
+	],
+	plugins: [
+		'@docusaurus/plugin-ideal-image',
+		[
+			'@docusaurus/plugin-content-docs',
+			{
+				id: 'administration',
+				path: 'administration',
+				routeBasePath: 'administration',
+				sidebarPath: require.resolve('./sidebarsAdmin.js'),
+				// ... other options
+			},
+		],
+	],
+}
