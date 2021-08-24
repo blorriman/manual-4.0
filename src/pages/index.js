eee0959d350062ecbbce590e1046d2437aa81a21
Bob Lorriman <blorriman@gmail.com>
2021-08-24 09:43:56 -0700

    working on Discussions

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,8 @@ export default function Home() {
 	const { siteConfig } = useDocusaurusContext()
 	return (
 		<Layout
-			title={`Hello from ${siteConfig.title}`}
+			title={siteConfig.title}
+			// title={`Hello from ${siteConfig.title}`}
 			description='democratic decision making for boards and committees'
 		>
 			<HomepageHeader />
